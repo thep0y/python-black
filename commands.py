879bf3656a420cf362d37e7a5a59c800c3d640e6
thepoy <thepoy@163.com>
2023-02-08 12:12:22 +0800

    upgrade pathspec to 0.11.0

--- commands.py
+++ commands.py
@@ -4,7 +4,7 @@
 # @Email:     thepoy@163.com
 # @File Name: commands.py
 # @Created:   2022-02-04 10:51:04
-# @Modified:  2022-12-17 19:29:30
+# @Modified:  2023-02-08 12:10:46
 
 import sublime
 import sublime_plugin
@@ -21,11 +21,12 @@ from .python_black.black import black_format
 from .python_black.mode import Mode
 from .python_black.log import child_logger
 from .python_black.utils import get_mode, set_mode
-from .python_black.lib.black import __version__
+from .python_black.lib.black import __version__ as black_version
+from .python_black.lib.pathspec._meta import __version__ as pathspec_version
 
 logger = child_logger(__name__)
 
-logger.info("black version: %s", __version__)
+logger.info("black version: %s, pathspec version: %s", black_version, pathspec_version)
 
 
 class BlackCommand(sublime_plugin.TextCommand):
--- python_black/lib/pathspec/__init__.py
+++ python_black/lib/pathspec/__init__.py
@@ -6,45 +6,71 @@ this only includes Git's wildmatch pattern matching (the style used for
 The following classes are imported and made available from the root of
 the `pathspec` package:
 
-- :class:`pathspec.gitignore.GitIgnoreSpec`
+-	:class:`pathspec.gitignore.GitIgnoreSpec`
 
-- :class:`pathspec.pathspec.PathSpec`
+-	:class:`pathspec.pathspec.PathSpec`
 
-- :class:`pathspec.pattern.Pattern`
+-	:class:`pathspec.pattern.Pattern`
 
-- :class:`pathspec.pattern.RegexPattern`
+-	:class:`pathspec.pattern.RegexPattern`
 
-- :class:`pathspec.util.RecursionError`
+-	:class:`pathspec.util.RecursionError`
 
 The following functions are also imported:
 
-- :func:`pathspec.util.lookup_pattern`
+-	:func:`pathspec.util.lookup_pattern`
 
 The following deprecated functions are also imported to maintain
 backward compatibility:
 
-- :func:`pathspec.util.iter_tree` which is an alias for
-  :func:`pathspec.util.iter_tree_files`.
+-	:func:`pathspec.util.iter_tree` which is an alias for
+	:func:`pathspec.util.iter_tree_files`.
 
-- :func:`pathspec.util.match_files`
+-	:func:`pathspec.util.match_files`
 """
 
-from .gitignore import GitIgnoreSpec
-from .pathspec import PathSpec
-from .pattern import Pattern, RegexPattern
-from .util import RecursionError, iter_tree, lookup_pattern, match_files
+from .gitignore import (
+	GitIgnoreSpec)
+from .pathspec import (
+	PathSpec)
+from .pattern import (
+	Pattern,
+	RegexPattern)
+from .util import (
+	RecursionError,
+	iter_tree,
+	lookup_pattern,
+	match_files)
 
 from ._meta import (
-    __author__,
-    __copyright__,
-    __credits__,
-    __license__,
-    __version__,
+	__author__,
+	__copyright__,
+	__credits__,
+	__license__,
+	__version__,
 )
 
 # Load pattern implementations.
 from . import patterns
 
-# Expose `GitIgnorePattern` class in the root module for backward
-# compatibility with v0.4.
+# DEPRECATED: Expose the `GitIgnorePattern` class in the root module for
+# backward compatibility with v0.4.
 from .patterns.gitwildmatch import GitIgnorePattern
+
+# Declare private imports as part of the public interface. Deprecated
+# imports are deliberately excluded.
+__all__ = [
+	'GitIgnoreSpec',
+	'PathSpec',
+	'Pattern',
+	'RecursionError',
+	'RegexPattern',
+	'__author__',
+	'__copyright__',
+	'__credits__',
+	'__license__',
+	'__version__',
+	'iter_tree',
+	'lookup_pattern',
+	'match_files',
+]
